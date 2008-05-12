f267978fe4adc4e8278a79f8bb96d9d82dfc4c74
Larry Diehl <larry@izea.com>
2008-05-11 20:15:13 -0400

    Added ParseTree.require_rlisp

--- lib/lisp_mode.rb
+++ lib/lisp_mode.rb
@@ -11,17 +11,25 @@ module Cry
     end
     
     module ClassMethods
-      def from_lisp(lisp)
-        lisp_tokenize lisp.scan(/[\(\)]|[^\s\)]+|\[\S*\]/)
+      def require_rlisp(file_path)
+        File.open(file_path) do |file|
+          ParseTree.from_rlisp(file.read).evaluate
+        end
       end
       
-      def lisp_tokenize(tokens, current_node = nil)
+      def from_rlisp(rlisp)
+        rlisp_tokenize rlisp.scan(/[\(\)]|[^\s\)]+|\[\S*\]/)
+      end
+      
+      def rlisp_tokenize(tokens, current_node = nil)
         while !tokens.empty? do
           case token = tokens.shift
           when "(" # new ParseTree, and next token is node method
-            next_node = ParseTree.new(tokens.shift.gsub("-", "_").to_sym)
+            rlisp_node_method = tokens.shift
+            rlisp_node_method.gsub!("-", "_") unless rlisp_node_method == "-"
+            next_node = ParseTree.new(rlisp_node_method.to_sym)
             if current_node
-              current_node << lisp_tokenize(tokens, next_node)
+              current_node << rlisp_tokenize(tokens, next_node)
             else
               current_node = next_node
             end
@@ -34,14 +42,13 @@ module Cry
       end
     end
     
-    def to_lisp
+    def to_rlisp
       parse_tree = []
       parse_tree << node_method.to_s.gsub("_", "-")
-      parse_tree << (node_object.is_a?(ParseTree) ? node_object.to_lisp : node_object.inspect )
-      parse_tree << node_arguments.map{|a| a.is_a?(ParseTree) ? a.to_lisp : a.inspect } unless node_arguments.empty?
+      parse_tree << (node_object.is_a?(ParseTree) ? node_object.to_rlisp : node_object.inspect)
+      parse_tree << node_arguments.map{|a| a.is_a?(ParseTree) ? a.to_rlisp : a.inspect } unless node_arguments.empty?
       "(#{parse_tree.join(" ")})"
     end
   end
 end
-# TODO: Cry.require_lisp
 # TODO: script/cry
--- spec/lisp_mode_spec.rb
+++ spec/lisp_mode_spec.rb
@@ -20,71 +20,87 @@ describe Object, "#e" do
   end
 end
 
-describe ParseTree, "#to_lisp" do  
+describe ParseTree, "#to_rlisp" do  
   it "should use parantheses instead of brackets, and not use colons or commas" do
     p = ParseTree.new(:+, 1, 2)
-    p.to_lisp.should == "(+ 1 2)"
+    p.to_rlisp.should == "(+ 1 2)"
   end
   
   it "should turn underscores in method names to dashes" do
     p = ParseTree.new(:to_s, 1)
-    p.to_lisp.should == "(to-s 1)"
+    p.to_rlisp.should == "(to-s 1)"
   end
   
   it "should work with ParseTree arguments" do
     p = ParseTree.new( :+, 1, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2) )
-    p.to_lisp.should == "(+ 1 (* (* 2 2) 2))"
+    p.to_rlisp.should == "(+ 1 (* (* 2 2) 2))"
   end
   
   it "should work with a ParseTree object" do
     p = ParseTree.new( :+, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2), 1 )
-    p.to_lisp.should == "(+ (* (* 2 2) 2) 1)"
+    p.to_rlisp.should == "(+ (* (* 2 2) 2) 1)"
   end
 end
 
-describe ParseTree, ".from_lisp" do  
+describe ParseTree, ".from_rlisp" do  
   it "should work with parantheses instead of brackets, and no colons or commas" do
     p = ParseTree.new(:+, 1, 2)
-    ParseTree.from_lisp("(+ 1 2)").should == p
+    ParseTree.from_rlisp("(+ 1 2)").should == p
   end
   
   it "should work with more than one character names" do
     p = ParseTree.new(:new, Array, 2, 1)
-    ParseTree.from_lisp("(new Array 2 1)").should == p
+    ParseTree.from_rlisp("(new Array 2 1)").should == p
   end
   
   it "should work with ParseTree arguments" do
     p = ParseTree.new( :+, 1, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2) )
-    ParseTree.from_lisp("(+ 1 (* (* 2 2) 2))").should == p
+    ParseTree.from_rlisp("(+ 1 (* (* 2 2) 2))").should == p
   end
   
   it "should work with ParseTree arguments, and spacing" do
     p = ParseTree.new( :+, 1, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2) )
-    ParseTree.from_lisp("(   + 1 (* (* 2    2) 2)  )").should == p
+    ParseTree.from_rlisp("(   + 1 (* (* 2    2) 2)  )").should == p
   end
   
   it "should work with a ParseTree object" do
     p = ParseTree.new( :+, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2), 1 )
-    ParseTree.from_lisp("(+ (* (* 2 2) 2) 1)").should == p
+    ParseTree.from_rlisp("(+ (* (* 2 2) 2) 1)").should == p
   end
   
   it "should work with a ParseTree object, and spacing" do
     p = ParseTree.new( :+, ParseTree.new(:*, ParseTree.new(:*, 2, 2), 2), 1 )
-    ParseTree.from_lisp(" ( + (* (  * 2 2 ) 2) 1  )").should == p
+    ParseTree.from_rlisp(" ( + (* (  * 2 2 ) 2) 1  )").should == p
   end
   
   it "should work with method names that have underscores" do
     p = ParseTree.new(:to_s, 1)
-    ParseTree.from_lisp("(to_s 1)").should == p
+    ParseTree.from_rlisp("(to_s 1)").should == p
   end
   
-  it "should turn dahses in method names to underscores" do
+  it "should turn dashes in method names to underscores" do
     p = ParseTree.new(:to_s, 1)
-    ParseTree.from_lisp("(to-s 1)").should == p
+    ParseTree.from_rlisp("(to-s 1)").should == p
+  end
+  
+  it "should not turn subtract into underscore" do
+    p = ParseTree.new(:-, 4, 2)
+    ParseTree.from_rlisp("(- 4 2)").should == p
   end
   
   it "should work with newline and tab characters" do
     p = ParseTree.new( :+, 1, ParseTree.new(:*, 2, 2) )
-    ParseTree.from_lisp("(+ 1 \n(* 2 2)\t)").should == p
+    ParseTree.from_rlisp("(+ 1 \n(* 2 2)\t)").should == p
+  end
+end
+
+describe ParseTree, ".require_rlisp" do
+  it "should use ParseTree.from_rlisp" do
+    ParseTree.should_receive(:from_rlisp).and_return(stub_everything("parse tree"))
+    ParseTree.require_rlisp(File.join(File.dirname(__FILE__), "fixtures", "example.rlisp"))
+  end
+  
+  it "should evaluae the code as Ruby with Lisp-like syntax" do
+    ParseTree.require_rlisp(File.join(File.dirname(__FILE__), "fixtures", "example.rlisp")).should == [4, 4, 4, 4, 4]
   end
 end
